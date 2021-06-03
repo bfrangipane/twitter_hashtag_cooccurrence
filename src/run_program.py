ffbe416a362972896b0d7c58e697467f5185926b
bfrangipane <frangipane.bret@berkeley.edu>
2021-06-03 09:17:30 -0700

    built out metadata

--- src/run_program.py
+++ src/run_program.py
@@ -13,63 +13,83 @@ def num_to_prob(col):
 
 def stop(hashtag_df, hashtags_searched, stopping_prob, iter_num, max_iters):
     if iter_num > max_iters:
-        stop = False
+        stop = True
     elif hashtags_searched == []:
         stop = False
     else:
         prob_df = hashtag_df.apply(num_to_prob)
         prob = prob_df[hashtags_searched].drop(hashtags_searched).max().max()
-        stop = (prob >= stopping_prob)
+        stop = (prob < stopping_prob)
     return stop, prob
 
 
-def create_metadata(seed_hashtag, stopping_prob=.2, max_iters=100):
+def create_metadata():
     metadata = {}
-    metadata['iters'] = {}
     metadata['init_time'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    metadata['seed_hashtag'] = seed_hashtag
-    metadata['stopping_prob'] = stopping_prob
-    metadata['max_iters'] = max_iters
+    metadata['num_searches'] = 0
+    metadata['searches'] = {}
+    metadata['hashtags_searched'] = []
     return metadata
 
 
-def write_to_file(seed_hashtag, next_hashtag, hashtag_df, hashtags_searched, tweet_df, stopping_prob, iter_num, max_iters, metadata):
-    rundata = {}
-    rundata['runtime'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    rundata['hashtag'] = next_hashtag
-    rundata['iter_num'] = iter_num
-    metadata['iter'][iter_num] = rundata
+def add_searchdata_to_metadata(metadata, seed_hashtag, stopping_prob=.2, max_iters=100):
+    searchdata = {}
+    searchdata['init_time'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    searchdata['seed_hashtag'] = seed_hashtag
+    searchdata['stopping_prob'] = stopping_prob
+    searchdata['max_iters'] = max_iters
+    searchdata['iters'] = {}
+    metadata['num_searches'] += 1
+    metadata['searches'][metadata['num_searches']] = searchdata
+    return metadata
+
+
+def add_iterdata_to_searchdata(next_hashtag, iter_num, searchdata):
+    iterdata = {}
+    iterdata['iter_num'] = iter_num
+    iterdata['hashtag'] = next_hashtag
+    iterdata['itertime'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    searchdata['iters'][iter_num] = iterdata
+    return searchdata
+
+
+def update_metadata(metadata, next_hashtag, hashtags_searched, iter_num):
+    searchdata = metadata['searches'][metadata['num_searches']]
+    searchdata = add_iterdata_to_searchdata(next_hashtag, iter_num, searchdata)
     metadata['hashtags_searched'] = hashtags_searched
-    metadata['num_iters'] = iter_num
+    return metadata
+
+
+def save_data(hashtag_df, tweet_df, metadata):
     with open('output/metadata.txt', 'w') as outfile:
         json.dump(metadata, outfile, indent=2)
     hashtag_df.to_csv('output/hashtag_df.csv')
     tweet_df.to_csv('output/tweet_df.csv')
-    return metadata
 
 
 def continue_search(seed_hashtag='', path_to_metadata='../output', stopping_prob=.2, max_iters=100, sleep_period=2):
-    path= '{}/metadata.txt'.format(path_to_metadata)
+    path = '{}/metadata.txt'.format(path_to_metadata)
     with open(path) as f:
         metadata = json.load(f)
     hashtags_searched = metadata['hashtags_searched']
     hashtag_df = pd.read_csv('../output/hashtag_df.csv')
     tweet_df = pd.read_csv('../output/tweet_df.csv')
-    next_hashtag, hashtag_df, hashtags_searched, tweet_df = run(seed_hashtag, hashtag_df=pd.DataFrame(), hashtags_searched=[], tweet_df=pd.DataFrame(), stopping_prob=.2, max_iters=100, sleep_period=2)
     seed_hashtag = process_hashtags.find_next_hashtag(tweet_df, hashtags_searched) if seed_hashtag == '' else seed_hashtag
+    metadata = add_searchdata_to_metadata(metadata, seed_hashtag, stopping_prob, max_iters)
+    next_hashtag, hashtag_df, hashtags_searched, tweet_df = run(seed_hashtag, hashtag_df, hashtags_searched, tweet_df, stopping_prob, max_iters, sleep_period, metadata)
     return next_hashtag, hashtag_df, hashtags_searched, tweet_df
 
 
-def run(seed_hashtag, hashtag_df=pd.DataFrame(), hashtags_searched=[], tweet_df=pd.DataFrame(), stopping_prob=.2, max_iters=100, sleep_period=2):
+def run(seed_hashtag, hashtag_df=pd.DataFrame(), hashtags_searched=[], tweet_df=pd.DataFrame(), stopping_prob=.2, max_iters=100, sleep_period=2, metadata=create_metadata()):
     next_hashtag = seed_hashtag
     iter_num = 1
-    metadata = create_metadata(seed_hashtag, stopping_prob, max_iters)
     while not stop(hashtag_df, hashtags_searched, stopping_prob, iter_num, max_iters, sleep_period):
         query = prepare_query.main(next_hashtag)
         json_files = search_tweets.main(query, next_hashtag)
         hashtags_searched.append(next_hashtag)
         next_hashtag, hashtag_df, tweet_df = process_hashtags.main(json_files, hashtags_searched, tweet_df, next_hashtag)
-        metadata = write_to_file(seed_hashtag, next_hashtag, hashtag_df, hashtags_searched, tweet_df, stopping_prob, iter_num, max_iters, metadata)
+        metadata = update_metadata(metadata, next_hashtag, hashtags_searched, iter_num)
+        save_data(hashtag_df, tweet_df, metadata)
         iter_num += 1
         time.sleep(sleep_period)
     return next_hashtag, hashtag_df, hashtags_searched, tweet_df
@@ -81,5 +101,10 @@ if __name__ == "__main__":
 
 # to do:
 # add the value at the instersection to the number for hashtag df?
+# -- the benefit is to use it to get accurate markov probs, which is essential
+# -- but need to make changes in the next hashtag function or the stopping criteria
+
+# wrap up metadata > searchdata > iterdata
+# each need to have complete picture of subprocesses including hashtags_searched
 
 # need some init.py to create directories
